8136ea72a429af50aa30e1d0ecc65bd288b4c7d7
hichamoujari <hichamoujari99@gmail.com>
2021-09-05 12:09:35 +0200

    by hicham

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wefix/screens/splashScreen.dart';
 
 void main() {
@@ -8,7 +9,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Wefix',
       theme: ThemeData(
         primarySwatch: Colors.blue,
